af5d9654ea2feb5e00015fefeb4ff421fd3f2c54
Oleh Predko <o.predko92@gmail.com>
2020-01-29 23:07:49 +0200

    added backup script

--- hometask_2/backup.sh
+++ hometask_2/backup.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-sudo find -type f -mtime -1 -exec cp {}  /home/che/Desktop/555 \;
+source_dir=/home/che/Documents/*
+backup_dir=/mnt/Virtual
+find $source_dir -mtime -1 -exec cp -r {} $backup_dir/$(date +%Y%m%d) \;
+
+
+
+
+#-----------------------------------with user input---------------------
+#echo Enter source directory
+#read source_dir
+#echo Enter destination directory
+#read backup_dir
+#find $source_dir  -mtime -1 -exec cp -r {} $backup_dir/$(date +%Y%m%d) \;
+
