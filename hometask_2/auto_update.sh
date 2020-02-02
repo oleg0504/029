22212f44c7a41ddf9ad6e7f0a6a7341719008799
Oleh Predko <o.predko92@gmail.com>
2020-02-02 22:25:01 +0200

    file

--- hometask_2/auto_update.sh
+++ hometask_2/auto_update.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo apt update -qq && sudo apt upgrade -qq -y > /var/log/update-$(date +%Y%m%d).log
+#sudo apt update -qq && sudo apt upgrade -qq -y > /var/log/update-$(date +%Y%m%d).log
+cronjob="0 0 * * 0 apt update -qq && apt upgrade -qq -y > /var/log/update-$(date +%Y%m%d).log"
+(crontab -u $USER -l; echo "$cronjob" ) | crontab -u $USER -
