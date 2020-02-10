f2ebced0793001f145c59bdb22f0cfa792c1d764
oleg0504 <49478622+oleg0504@users.noreply.github.com>
2020-02-10 13:20:00 +0200

    hometask_3

--- hometask_3/test.sh
+++ hometask_3/test.sh
@@ -5,17 +5,22 @@ if test -f "$FILE"; then
 
     apt-get -y update
     apt-get -y install mysql-server
+	sudo usermod -aG sudo vagrant
     sudo systemctl start mysql.service
-    #sudo systemctl enable mysql.service
     sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
     FLUSH PRIVILEGES;
-    CREATE DATABASE CONFLUENCE;
+    CREATE DATABASE CONFLUENCE CHARACTER SET utf8 COLLATE utf8_bin;
     GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '123456' WITH GRANT OPTION;"
     sudo sed -i "s/.*bind-address.*/bind-address =      0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+	sudo echo transaction-isolation=READ-COMMITTED >> /etc/mysql/mysql.conf.d/mysqld.cnf
     sudo systemctl restart mysql.service
     chmod a+x atlassian-confluence-7.3.1-x64.bin
-    sudo ./atlassian-confluence-7.3.1-x64.bin -q -varfile confluence-response.varfile
-
+    ./atlassian-confluence-7.3.1-x64.bin -q -varfile confluence-response.varfile
+	#DOWNLOAD AND COPY MYSQL JDBC DRIVER
+	wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.48.tar.gz
+	tar -xvf mysql-connector-java-5.1.48.tar.gz
+	cp /home/vagrant/mysql-connector-java-5.1.48/mysql-connector-java-5.1.48.jar /opt/atlassian/confluence/lib
+	sudo systemctl start confluence.service
 else 
     echo $FILE DOES NOT EXIST
 fi
